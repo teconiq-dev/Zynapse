cee2b0ce6f9ed4c45c4386d41e2e232b6f371a45
jude7733 <judeputhussery360@gmail.com>
2024-12-08 19:35:40 +0530

    init nextjs with countdown and header

---
+++ src/components/App.tsx
@@ -0,0 +1,72 @@
+"use client";
+import "../css/App.css";
+import Scene from "./Scene";
+import { Canvas } from "@react-three/fiber";
+import { Leva } from "leva";
+import { CountdownTimer } from "./countdown";
+
+function Title3D() {
+  return (
+    <div className="absolute top-0 left-0 h-screen w-screen">
+      <Canvas
+        dpr={[1, 2]}
+        gl={{
+          antialias: true,
+          preserveDrawingBuffer: true,
+        }}
+        camera={{
+          fov: 55,
+          near: 0.1,
+          far: 200,
+        }}
+      >
+        <Scene />
+      </Canvas>
+    </div>
+  );
+}
+
+function HeaderComponent() {
+  return (
+    <header className="relative z-50 mx-7 flex max-lg:flex-col justify-between py-6 border-b border-white/60 pointer-events-auto">
+      <div className="whitespace-nowrap">
+        <h1 className="font-bold inline align-middle mr-2">Zynapse</h1>
+      </div>
+    </header>
+  );
+}
+
+function Aside() {
+  return (
+    <div className="absolute top-1/2 right-16 text-lg">
+      <CountdownTimer />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <Leva
+        collapsed={false}
+        flat={true}
+        hidden
+        theme={{
+          sizes: {
+            titleBarHeight: "28px",
+          },
+          fontSizes: {
+            root: "10px",
+          },
+        }}
+      />
+      <main className="font-sans">
+        <HeaderComponent />
+        <Title3D />
+        <Aside />
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Scene.tsx
@@ -0,0 +1,118 @@
+"use client";
+import { useRef, useMemo, useEffect, useState } from "react";
+import { useControls } from "leva";
+import { debounce } from "lodash";
+
+// 3D
+import * as THREE from "three";
+import { PointLightHelper } from "three";
+import { useFrame, useThree } from "@react-three/fiber";
+import { useHelper, Html } from "@react-three/drei";
+import CustomShaderMaterial from "three-custom-shader-material";
+import vertexShader from "./shaders/vertex.glsl";
+import fragmentShader from "./shaders/fragment.glsl";
+import html2canvas from "html2canvas";
+
+const useDomToCanvas = (domEl) => {
+  const [texture, setTexture] = useState();
+  useEffect(() => {
+    if (!domEl) return;
+    const convertDomToCanvas = async () => {
+      const canvas = await html2canvas(domEl, { backgroundColor: null });
+      setTexture(new THREE.CanvasTexture(canvas));
+    };
+
+    convertDomToCanvas();
+
+    const debouncedResize = debounce(() => {
+      convertDomToCanvas();
+    }, 100);
+
+    window.addEventListener("resize", debouncedResize);
+    return () => {
+      window.removeEventListener("resize", debouncedResize);
+    };
+  }, [domEl]);
+
+  return texture;
+};
+
+function Lights() {
+  const pointLightRef = useRef();
+
+  useHelper(pointLightRef, PointLightHelper, 0.7, "cyan");
+
+  const config = useControls("Lights", {
+    color: "#ffffff",
+    intensity: { value: 30, min: 0, max: 5000, step: 0.01 },
+    distance: { value: 12, min: 0, max: 100, step: 0.1 },
+    decay: { value: 1, min: 0, max: 5, step: 0.1 },
+    position: { value: [2, 4, 6] },
+  });
+  return <pointLight ref={pointLightRef} {...config} />;
+}
+
+function Scene() {
+  const state = useThree();
+  const { width, height } = state.viewport;
+  const [domEl, setDomEl] = useState(null);
+
+  const materialRef = useRef();
+  const textureDOM = useDomToCanvas(domEl);
+
+  const uniforms = useMemo(
+    () => ({
+      uTexture: { value: textureDOM },
+      uMouse: { value: new THREE.Vector2(0, 0) },
+    }),
+    [textureDOM],
+  );
+
+  const mouseLerped = useRef({ x: 0, y: 0 });
+
+  useFrame((state, delta) => {
+    const mouse = state.mouse;
+    mouseLerped.current.x = THREE.MathUtils.lerp(
+      mouseLerped.current.x,
+      mouse.x,
+      0.1,
+    );
+    mouseLerped.current.y = THREE.MathUtils.lerp(
+      mouseLerped.current.y,
+      mouse.y,
+      0.1,
+    );
+    materialRef.current.uniforms.uMouse.value.x = mouseLerped.current.x;
+    materialRef.current.uniforms.uMouse.value.y = mouseLerped.current.y;
+  });
+
+  return (
+    <>
+      <Html zIndexRange={[-1, -10]} prepend fullscreen>
+        <div ref={(el) => setDomEl(el)} className="dom-element">
+          <p className="flex flex-col">
+            WHEN <br />
+            WILL <br />
+            WE <br />
+            MEET ?<br />
+          </p>
+        </div>
+      </Html>
+      <mesh>
+        <planeGeometry args={[width, height, 254, 254]} />
+        <CustomShaderMaterial
+          ref={materialRef}
+          baseMaterial={THREE.MeshStandardMaterial}
+          vertexShader={vertexShader}
+          fragmentShader={fragmentShader}
+          uniforms={uniforms}
+          flatShading
+          silent
+        />
+        <Lights />
+      </mesh>
+    </>
+  );
+}
+
+export default Scene;
