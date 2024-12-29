c8d2c156b042362d3cff3f6b58a4357e9b70c972
jude7733 <judeputhussery360@gmail.com>
2024-12-30 01:20:52 +0530

    hide box in sm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import CountdownTimer from "@/components/countdown";
 import { Heading } from "@/components/heading";
 import { About } from "@/components/about";
-import { Boxes } from "@/components/ui/background-boxes";
 import MorphingText from "@/components/ui/morphing-text";
+import { Boxes } from "@/components/ui/background-boxes";
 
 export default function Home() {
   const texts = [
@@ -18,15 +18,19 @@ export default function Home() {
   ];
 
   return (
-    <div className="flex flex-col items-start justify-start w-full gap-6 p-3 md:p-14 bg-[url('/mesh-gradient.png')] bg-cover">
-      <Heading />
-      <Boxes />
-      <div className="flex justify-between items-center flex-wrap-reverse w-full gap-14 mb-20">
-        <MorphingText texts={texts} />
-        <CountdownTimer />
+    <>
+      <div className="flex flex-col items-start justify-start w-full max-h-screen gap-6 px-3 md:px-14 pt-5 md:pt-14 bg-[url('/mesh-gradient.png')] bg-cover">
+        <Boxes className="hidden lg:flex" />
+        <Heading />
+        <div className="flex justify-between items-center flex-wrap-reverse w-full gap-14 mb-20">
+          <MorphingText texts={texts} />
+          <CountdownTimer />
+        </div>
+        <div className="divider divider-secondary z-10"></div>
       </div>
-      <div className="divider divider-secondary z-10"></div>
-      <About />
-    </div>
+      <div className="bg-base-100 py-10 px-2 md:px-10">
+        <About />
+      </div>
+    </>
   );
 }
