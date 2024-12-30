d21551440d4c99f96c9c8aa600947aacf69fb02d
jude7733 <judeputhussery360@gmail.com>
2024-12-30 16:07:50 +0530

    hyperspeed bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import CountdownTimer from "@/components/countdown";
-import { Heading } from "@/components/heading";
-import { About } from "@/components/about";
+import {Heading} from "@/components/heading";
+import {About} from "@/components/about";
 import MorphingText from "@/components/ui/morphing-text";
-import { Boxes } from "@/components/ui/background-boxes";
+import {Boxes} from "@/components/ui/background-boxes";
+import {Cover} from "@/components/ui/cover";
 
 export default function Home() {
   const texts = [
@@ -20,18 +21,17 @@ export default function Home() {
 
   return (
     <>
-      <div className="flex flex-col items-start justify-start w-full min-h-screen gap-6 px-3 md:px-14 pt-5 md:pt-14 bg-[url('/mesh-gradient.png')] bg-cover">
-        <Boxes className="hidden lg:flex" />
-        <Heading />
+      <Heading/>
+      <div className="flex flex-col items-start justify-start w-full min-h-screen gap-6 pt-5 md:pt-14">
         <div className="flex justify-between items-center flex-wrap w-full gap-48 mb-20">
-          <MorphingText texts={texts} />
-          <CountdownTimer />
+          <MorphingText texts={texts}/>
+          <CountdownTimer/>
         </div>
-        <div className="divider divider-secondary z-10 my-0 pb-0"></div>
       </div>
       <div className="bg-base-100 py-10 px-2 md:px-10">
-        <About />
+        <About/>
       </div>
     </>
-  );
+  )
+    ;
 }
