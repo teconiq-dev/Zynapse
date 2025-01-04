32a36fbafd4816b5fef5c709e7a1af3aff236985
Stalino A J <stalinosmj@outlook.com>
2025-01-04 23:16:10 +0530

    basic loading

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Loading() {
+    return (
+        <div className="flex flex-col items-center justify-center w-full h-full">
+            <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue-500 mb-4"></div>
+            <p className="text-lg font-medium">Loading, please wait...</p>
+        </div>
+    );
+}
