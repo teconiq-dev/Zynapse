0629d23b1975a56dfdc536a12fc3f079ef1c16a7
jude7733 <judeputhussery360@gmail.com>
2025-01-05 01:43:11 +0530

    fix nav links and loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
-
 export default function Loading() {
-    return (
-        <div className="flex flex-col items-center justify-center w-full h-full">
-            <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue-500 mb-4"></div>
-            <p className="text-lg font-medium">Loading, please wait...</p>
-        </div>
-    );
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full min-h-svh">
+      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue-500 mb-4"></div>
+      <p className="text-lg font-medium">Loading, please wait...</p>
+    </div>
+  );
 }
