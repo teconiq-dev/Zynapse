2815bfe19d3c31982d64f97a3f2c4c17cff4f30d
jude7733 <judeputhussery360@gmail.com>
2025-01-06 16:17:50 +0530

    bg changed

--- src/components/events-card.tsx
+++ src/components/events-card.tsx
@@ -52,9 +52,9 @@ type EventCardProps = {
       {loading ? (
         <div className="skeleton h-64 w-full"></div>
       ) : (
-        <CardContainer className="z-30">
+        <CardContainer className="z-10">
           <CardBody
-            className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto h-auto rounded-xl p-6 border  ">
+            className="relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] bg-black/10 backdrop-blur dark:border-white/[0.2] border-black/[0.1] w-auto h-auto rounded-xl p-6 border  ">
             <CardItem
               translateZ="50"
               className="text-xl font-bold text-neutral-600 dark:text-white"
