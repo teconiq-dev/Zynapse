b4f1f6b37043661202b1aa1f208879274c720d8b
Stalino A J <stalinosmj@outlook.com>
2025-01-17 21:26:58 +0530

    new images

--- src/lib/workshops.ts
+++ src/lib/workshops.ts
@@ -1,48 +1,27 @@
-export const eventsList = [
-  "Power BI",
-  "UI/UX",
-  "Blockchain",
-  "Unity",
-];
+export const eventsList = ["Power BI", "UI/UX", "Blockchain", "Unity",];
 
-export const individualEvents = [
-  {
-    id: 1,
-    title: "Power BI",
-    description: "Learn new skills with Power BI",
-    imageSrc: "/PoweBiWorkshop.jpeg",
-    rules: {
-      do: [""],
-      dont: [""],
+export const individualEvents = [{
+    id: 1, title: "Power BI", description: "Learn new skills with Power BI", imageSrc: "/PoweBiWorkshop.jpeg", rules: {
+        do: [""], dont: [""],
     },
-  },
-  {
+}, {
     id: 2,
     title: "UI/UX",
     description: "Fundamentals of UI/UX design to craft a seamless experience and build your dream product!",
     imageSrc: "/UIUXWorkshop.jpeg",
     rules: {
-      do: [""],
+        do: [""],
     },
-  },
-  {
+}, {
     id: 3,
     title: "Blockchain",
     description: "Introduction to Blockchain fundamentals",
     imageSrc: "/BlockchainWorkshop.jpeg",
     rules: {
-      do: ["Do this", "Do that"],
-      dont: ["Don't do this", "Don't do that"],
+        do: ["Do this", "Do that"], dont: ["Don't do this", "Don't do that"],
     },
-  },
-  {
-    id: 4,
-    title: "Unity",
-    description: "Make your own games with Unity.",
-    imageSrc: "/UnityWorkshop.jpeg",
-    rules: {
-      do: [""],
-      dont: [""],
+}, {
+    id: 4, title: "Unity", description: "Make your own games with Unity.", imageSrc: "/UnityWorkshop.jpeg", rules: {
+        do: [""], dont: [""],
     },
-  },
-];
+},];
